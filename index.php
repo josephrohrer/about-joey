54edf0b79a6429558f7679ba4a610da7c4a07ba3
Joseph Rohrer <josephrohrer@me.com>
2017-01-10 08:20:54 -0700

    Created index page.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> About Me - Joey Rohrer</title>
+		<style>
+			h1 { font-size:240%; color:red; }
+		</style>
+	</head>
+	<body>
+		<h1>Hi, I'm Joey.</h1>
+		<br>
+		<h2>Professional</h2>
+		<p>I love technology. I'm currently working part-time at Best Buy selling computers.
+			I have a Bachelor's degree in Business Marketing from the University of New Mexico and a Master's in Business Administration from the University of Phoenix. I have experience in marketing, graphic design, sales and overall bad assery.
+			<br>
+			I like to tinker. I recently built a pretty bad ass home theater PC and have been spending a lot of time configuring that. A couple of projects I want to start soon are: 1) Building a RetroPie, 2) Re-building my gaming computer, and 3) Building a FreeNAS Storage Server.
+		</p>
+		<br>
+		<h2>Personal</h2>
+		<p>I've lived in Albuquerque for about 20 years. Not wifed up yet, but I have two dogs that like to cuddle so that will suffice for now. I like to play volleyball and softball to stay active and for fun.</p>
+		<img src="images/15800238_1487913697902781_8730589775209769033_o.jpg">
+		<br><b>This is a picture of my volleyball team that took 2nd place in a volleyball tournament last week!</b>
+	</body>
+</html>
